ed7c53038407839bc27f71a0cd93c4e7afc8d6c9
Hugo Azevedo <azevhugo@amazon.com>
2026-08-24 00:29:27 -0300

    publica site 2026-08-24

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3656613378" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2039332059" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
